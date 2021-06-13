fe5d1654fbfb1f506ba34f9a94b3205609094543
112446 <112446@emmauscollege.nl>
2021-06-13 18:08:47 +0000

    ssa
    Signed-off-by: 112446 <112446@emmauscollege.nl>

--- script.js
+++ script.js
@@ -142,8 +142,8 @@ var checkGameOver = function() {
 var doodSpeler = function () {
     /* speler verliest punten als tegen de rand komt.*/
     if (spelerX<0 || spelerX>1280 || spelerY<0 || spelerY>720)
-        score -= 1;
-        text("Je hebt een punt verloren.", 500,50,250,100);
+        score=0;
+        text("Je verliest al je punten als je tegen de rand komt.", 500,50,250,100);
 }
 
 
